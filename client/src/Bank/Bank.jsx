6d6e694d11172075cc6542ea66ef8ad3dc2b8222
Mohdhasnainn <themohdhasnain@gmail.com>
2023-09-07 22:19:35 -0700

    .

--- client/src/Bank/Bank.jsx
+++ client/src/Bank/Bank.jsx
@@ -2,9 +2,10 @@ import React, { useEffect, useState } from "react";
 import { useAuthContext } from "../Contexts/AuthContext";
 import axios from "axios";
 import Cookies from "js-cookie";
+import { GiFastBackwardButton } from "react-icons/gi";
 
 const Bank = () => {
-  const { userdata } = useAuthContext();
+  const { userdata, user } = useAuthContext();
   const [slide, setSlide] = useState(0);
   const [clas, setclas] = useState("");
   const [subdata, setSubdata] = useState([]);
@@ -12,14 +13,14 @@ const Bank = () => {
   const [questions, setQuestions] = useState([]);
   const [sloading, setSLoading] = useState(false);
   const [tab, setTab] = useState("mcq");
+  const [subject, setsubject] = useState(userdata.subject);
 
-  const FETCH = async (cls) => {
+  const FETCH = async (cls, subj) => {
     setSLoading(true);
+    setsubject(subj);
     const { data } = await axios.get(
       import.meta.env.VITE_URL +
-        `/api/bank/chapters?class=${cls}&subject=${userdata.subject
-          .toUpperCase()
-          .trim()}`,
+        `/api/bank/chapters?class=${cls}&subject=${subj.toUpperCase().trim()}`,
       {
         headers: {
           "Content-Type": "application/json",
@@ -31,10 +32,10 @@ const Bank = () => {
     setSLoading(false);
   };
 
-  const FetchQuestions = async (cls, chp) => {
+  const FetchQuestions = async (cls, chp, subj) => {
     const { data } = await axios.get(
       import.meta.env.VITE_URL +
-        `/api/bank/findqno?class=${cls}&subject=${userdata.subject
+        `/api/bank/findqno?class=${cls}&subject=${subj
           .toUpperCase()
           .trim()}&chapter=${chp}`,
       {
@@ -75,10 +76,10 @@ const Bank = () => {
   };
 
   useEffect(() => {
-    if (userdata && userdata.classs !== "Both") {
-      const cls = userdata.classs.split(" ")[0];
+    if (userdata && userdata.classs !== "Both" && user.isAdmin !== true) {
+      const cls = userdata?.classs.split(" ")[0];
       setclas(userdata.classs);
-      FETCH(cls);
+      FETCH(cls, userdata.subject);
       setSlide(1);
     }
   }, []);
@@ -96,7 +97,7 @@ const Bank = () => {
                   className="class_card rounded border py-3 px-2 w-25 me-4 fs-5 text-center"
                   type="button"
                   onClick={() => {
-                    FETCH("9");
+                    FETCH("9", userdata.subject);
                     setclas("9 class");
                     setSlide(1);
                   }}
@@ -107,7 +108,7 @@ const Bank = () => {
                   className="class_card rounded border py-3 px-2 w-25 fs-5  text-center"
                   type="button"
                   onClick={() => {
-                    FETCH("10");
+                    FETCH("10", userdata.subject);
                     setclas("10 class");
                     setSlide(1);
                   }}
@@ -117,6 +118,140 @@ const Bank = () => {
               </div>
             </div>
           ) : (
+            <div>
+              <h2 className="fw-bold text-center">Please Choose class</h2>
+              <div className="d-flex align-items-center justify-content-center mt-3">
+                <div
+                  className="class_card rounded border py-3 px-2 w-25 me-4 fs-5 text-center"
+                  type="button"
+                  onClick={() => {
+                    // FETCH("9");
+                    setclas("9 class");
+                    setSlide(4);
+                  }}
+                >
+                  9 class
+                </div>
+                <div
+                  className="class_card rounded border py-3 px-2 w-25 fs-5  text-center"
+                  type="button"
+                  onClick={() => {
+                    // FETCH("10");
+                    setclas("10 class");
+                    setSlide(4);
+                  }}
+                >
+                  10 class
+                </div>
+              </div>
+            </div>
+          )}
+        </>
+      )}
+
+      {slide === 4 && (
+        <>
+          {clas === "9 class" ? (
+            <div className="px-5">
+              <GiFastBackwardButton
+                size={25}
+                className="back"
+                cursor={"pointer"}
+                onClick={() => setSlide(0)}
+              />
+              <h1 className="text-center fw-bold">Class 9 Subjects</h1>
+              <p
+                className="fs-5 mt-1 px-3 py-2 bg-gray rounded mt-2"
+                type="button"
+                onClick={(e) => {
+                  FETCH("9", e.target.innerHTML);
+                  setSlide(1);
+                }}
+              >
+                English
+              </p>
+              <p
+                className="fs-5 mt-1 px-3 py-2 bg-gray rounded"
+                type="button"
+                onClick={(e) => {
+                  FETCH("9", e.target.innerHTML);
+                  setSlide(1);
+                }}
+              >
+                Urdu
+              </p>
+              <p
+                className="fs-5 mt-1 px-3 py-2 bg-gray rounded"
+                type="button"
+                onClick={(e) => {
+                  FETCH("9", e.target.innerHTML);
+                  setSlide(1);
+                }}
+              >
+                Maths
+              </p>
+              <p
+                className="fs-5 mt-1 px-3 py-2 bg-gray rounded"
+                type="button"
+                onClick={(e) => {
+                  FETCH("9", e.target.innerHTML);
+                  setSlide(1);
+                }}
+              >
+                Physics
+              </p>
+              <p
+                className="fs-5 mt-1 px-3 py-2 bg-gray rounded"
+                type="button"
+                onClick={(e) => {
+                  FETCH("9", e.target.innerHTML);
+                  setSlide(1);
+                }}
+              >
+                Chemistry
+              </p>
+              <p
+                className="fs-5 mt-1 px-3 py-2 bg-gray rounded"
+                type="button"
+                onClick={(e) => {
+                  FETCH("9", e.target.innerHTML);
+                  setSlide(1);
+                }}
+              >
+                Biology
+              </p>
+              <p
+                className="fs-5 mt-1 px-3 py-2 bg-gray rounded"
+                type="button"
+                onClick={(e) => {
+                  FETCH("9", e.target.innerHTML);
+                  setSlide(1);
+                }}
+              >
+                Computer
+              </p>
+              <p
+                className="fs-5 mt-1 px-3 py-2 bg-gray rounded"
+                type="button"
+                onClick={(e) => {
+                  FETCH("9", e.target.innerHTML);
+                  setSlide(1);
+                }}
+              >
+                PST
+              </p>
+              <p
+                className="fs-5 mt-1 px-3 py-2 bg-gray rounded"
+                type="button"
+                onClick={(e) => {
+                  FETCH("9", e.target.innerHTML);
+                  setSlide(1);
+                }}
+              >
+                Islamiat
+              </p>
+            </div>
+          ) : (
             <div></div>
           )}
         </>
@@ -124,8 +259,14 @@ const Bank = () => {
 
       {slide === 1 && (
         <div className="mt-3">
+          <GiFastBackwardButton
+            size={25}
+            className="back"
+            cursor={"pointer"}
+            onClick={() => setSlide(user.isAdmin ? 4 : 0)}
+          />
           <h1 className="text-center fw-bold">
-            {userdata.classs}, {userdata.subject}
+            {clas}, {subject}
           </h1>
           {sloading ? (
             <p className="text-center">Loading</p>
@@ -136,7 +277,7 @@ const Bank = () => {
                   type="button"
                   className="d-flex justify-content-between align-items-center chapter px-3 py-2 bg-gray fs-5 rounded mt-2"
                   key={elem}
-                  onClick={() => FetchQuestions(clas, elem)}
+                  onClick={() => FetchQuestions(clas, elem, subject)}
                 >
                   Chapter {i + 1} : {elem}
                 </div>
@@ -148,6 +289,12 @@ const Bank = () => {
 
       {slide === 2 && (
         <div className="mt-3">
+           <GiFastBackwardButton
+            size={25}
+            className="back"
+            cursor={"pointer"}
+            onClick={() => setSlide(1)}
+          />
           <div className="d-flex align-items-center justify-content-between mx-auto w-75 mt-3">
             <div
               className="class_card rounded border py-3 px-2 w-25 me-4 fs-5 
@@ -203,18 +350,20 @@ const Bank = () => {
                         <span className="fs-6 fw-bold"> {i + 1}.</span>{" "}
                         {elem.qno}
                       </p>
-                      {elem?.options &&
-                        elem.options.map((e, index) => {
-                          const sno = ["A", "B", "C", "D"];
-                          return (
-                            <p className="mt-1 fs-5" key={e}>
-                              <span className="fs-6 fw-bold text-muted">
-                                {sno[index]}.
-                              </span>{" "}
-                              {e}
-                            </p>
-                          );
-                        })}
+                      <div className="d-flex justify-content-between flex-wrap w-100 align-items-center">
+                        {elem?.options &&
+                          elem.options.map((e, index) => {
+                            const sno = ["a", "b", "c", "d"];
+                            return (
+                              <p className="mt-1 fs-5 w-50" key={e}>
+                                <span className="fs-6 fw-bold text-muted">
+                                  {sno[index]}.
+                                </span>{" "}
+                                {e}
+                              </p>
+                            );
+                          })}
+                      </div>
                     </div>
                   );
                 })
